7e2fce43af355c494ca806bbfd963fadbf27e445
Rastogi <Nirmesh.Rastogi@dell.com>
2023-01-31 20:12:48 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ const App = () => {
 
 	useEffect(() => {
 		try{
-		axios.get('https://node-postgres-crud-api-nirmesh44-dev.apps.sandbox-m3.1530.p1.openshiftapps.com/users')
+			axios.get('http://node-postgres-crud-api-nirmesh-openshift-crud.apps.ocp4.ds.blr.lab/users')
+		//axios.get('https://node-postgres-crud-api-nirmesh44-dev.apps.sandbox-m3.1530.p1.openshiftapps.com/users')
 		//axios.get('http://172.30.193.9/users')
 		//axios.get('http://node-postgres-crud-api/users')
 		.then((response) => {
